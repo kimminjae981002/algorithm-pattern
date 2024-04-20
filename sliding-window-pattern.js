24f67fa01c384daccfd6a3fbda37fcb36daec1a7
minjae Kim <cmg981548@gmail.com>
2024-04-20 20:40:21 +0900

    feat: [main] 알고리즘 기준점 간 이동 배열 패턴

--- sliding-window-pattern.js
+++ sliding-window-pattern.js
@@ -3,22 +3,51 @@
 // 내 코드
 function maxSubarraySum(arr, n) {
     arr.sort();
-    return arr[arr.length - 1] + n 
+    return arr[arr.length - 1] + n
 }
 
 console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2))
 console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4))
 
+// n의 인자 수 만큼 합을 더 했을 때 마주보는 숫자들이 가장 높은 것
+// 연습 코드
 function maxSubarraySum(arr, n) {
-    if (n > arr.length) {
-        return null;
+    
+    const sortArr = arr.slice().sort();
+    const max = arr.indexOf(sortArr[sortArr.length - 1]);
+    
+    let sum1 = 0;
+    let sum2 = 0;
+    
+    for (let i = 0; i < arr.length; i++){
+        sum1 = arr[max] + arr[max - 1];
+        sum2 = arr[max] + arr[max + 1];
+    };
+
+    return sum1 > sum2 ? sum1 : sum2;
+
+}
+
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2))
+console.log(maxSubarraySum([1,2,5,2,8,1,5],4))
+
+// 슬라이딩 윈도우
+function maxSubarraySum(arr, n) {
+    let tempSum = 0;
+    let maxSum = 0;
+
+    // 0번부터 n번까지 합을 넣어준다.
+    for (let i = 0; i < n; i++){
+        tempSum += arr[i]
     }
 
-    let max = -Infinity;
-
-    for (let i = 0; i < arr.length - n + 1; i++){
-        
+    for (let i = n; i < arr.length; i++){
+        tempSum = tempSum - arr[i - n] + arr[i];
+        // 0번부터 n번까지 합에서 0번을 빼고 n번을 더한다.
+        maxSum = Math.max(maxSum, tempSum);
     }
+
+    return maxSum;
 }
 
 console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2))
