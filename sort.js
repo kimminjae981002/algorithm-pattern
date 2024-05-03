f0dd1eeeb94c6a39e6a5ee37fe286906859cd2f2
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2024-05-03 14:36:42 +0900

    feat: [main] 선택 정렬

--- sort.js
+++ sort.js
@@ -74,3 +74,43 @@ function sort(arr) {
 }
 
 console.log(sort([5, 8, 61, 2]));
+
+// 선택 정렬
+
+function selectionSort(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    var lowest = i;
+    for (var j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+    if (i !== lowest) {
+      //SWAP!
+      var temp = arr[i];
+      arr[i] = arr[lowest];
+      arr[lowest] = temp;
+    }
+  }
+  return arr;
+}
+
+// ES2015 VERSION
+function selectionSort(arr) {
+  const swap = (arr, idx1, idx2) =>
+    ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]);
+
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[lowest] > arr[j]) {
+        lowest = j;
+      }
+    }
+    if (i !== lowest) swap(arr, i, lowest);
+  }
+
+  return arr;
+}
+
+console.log(selectionSort([0, 2, 34, 22, 10, 19, 17]));
